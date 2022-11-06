e877b6bbbde4a9f9c8524aff2ed773f8ccdc1e65
Cooper Hall <cooperhall111@gmail.com>
2022-11-06 16:48:00 -0500

    commit tree 06c84f2829ec93771c2895cb5c32a347da17ada1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+//function upperCaseCustomerName () {
+  //  return customerName.toUpperCase('') }
+function upperCaseCustomerName() {
+  var name = customerName.toUpperCase()
+  return name
+}
+
+
+
+//function overwriteBestCustomer () {
+  //  return (var bestCustomer = "maybe bob")
+//}
